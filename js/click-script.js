8535214f7cac3d203cb2989c6d7bcd0953146327
Muyiwa Olaniyan <moo@mootoo.co>
2021-11-15 00:48:11 -0500

    Added comment to js

--- js/click-script.js
+++ js/click-script.js
@@ -1,3 +1,4 @@
+// This function changes the style display parameter on the button when clicked and allows it to be visible
 function openSesame() {
     document.getElementById("clickMessage").style.display = 'block';
 }
