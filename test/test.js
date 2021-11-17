a30aa8c39319357a48bb20bf283b2ea6669d3370
Moo Olaniyan <moo@mootoo.co>
2021-11-17 09:03:46 +0000

    Initial commit for homelab-deploy branch with deployment over ssh

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@
 
 const fs = require("fs");
 const path = require('path');
-const html = fs.readFileSync(path.resolve(__dirname,"../home.html"), 'utf8');
+const html = fs.readFileSync(path.resolve(__dirname,"../index.html"), 'utf8');
 
 jest
     .dontMock('fs');
