2ad45269b5d59886ffc74f7d209fd9447f5a0707
Moo Olaniyan <moo@mootoo.co>
2021-11-17 07:43:50 +0000

    Cleaned up code a bit, switched click event to call of openSesame() function since click was inconsistent

--- js/click-script.js
+++ js/click-script.js
@@ -1,13 +1,12 @@
 // This function inserts the text upon being called, and then disables the click button
 const openSesame = () => {
     var text = "Thank you for considering my application!";
-    window.document.getElementById("clickMessage").innerHTML = text;
-    window.document.getElementById("clickButton").disabled = true;
+    document.getElementById("clickMessage").innerHTML = text;
+    document.getElementById("clickButton").disabled = true;
 }
 
 window.onload = function() {
-    const callClick = window.document.getElementById("clickButton");
-    callClick.addEventListener("click", openSesame);
+    document.getElementById("clickButton").addEventListener("click", openSesame);
 }
 
 module.exports = openSesame;
--- test/test.js
+++ test/test.js
@@ -2,18 +2,18 @@
 
 const fs = require("fs");
 const path = require('path');
-const html = fs.readFileSync(path.resolve(__dirname,"../index.html").toString(), 'utf8');
+const html = fs.readFileSync(path.resolve(__dirname,"../index.html"), 'utf8');
 
 jest
     .dontMock('fs');
 
 describe("Button Test", function() {
     beforeEach(() => {
-	require('../js/click-script.js');
-        window.document.body.innerHTML = html.toString()
-	const clickMessage = window.document.getElementById('clickMessage');
-        const clickButton = window.document.getElementById('clickButton');
-	clickButton.click();
+	const openSesame = require('../js/click-script');
+        document.body.innerHTML = html.toString();
+//	document.getElementById('clickButton').click();
+	openSesame();
+	const clickMessage = document.getElementById('clickMessage').innerHTML;
     });
     afterEach(() => {
         jest.resetModules();
@@ -22,6 +22,6 @@ describe("Button Test", function() {
         expect(clickMessage.innerHTML).toBe("Thank you for considering my application!");
     });
     it("Button Click Test", function() {
-        expect(clickButton.disabled).toBe(true);
+        expect(global.document.getElementById('clickButton').disabled).toBe(true);
     });
 });
