e39238ed8bf031c0e6ad3226b3be0f960d84badb
Moo Olaniyan <moo@mootoo.co>
2021-11-17 05:12:03 +0000

    Updated testing framework to jest, added .circleci config for build, updated click-script.js to use the DOM, additionally made click-script more testable

--- js/click-script.js
+++ js/click-script.js
@@ -1,7 +1,13 @@
 // This function inserts the text upon being called, and then disables the click button
-function openSesame() {
+const openSesame = () => {
     var text = "Thank you for considering my application!";
-    document.getElementById("clickMessage").textContent += text;
-    document.getElementById("clickButton").disabled = true;
-    return text;
+    window.document.getElementById("clickMessage").innerHTML = text;
+    window.document.getElementById("clickButton").disabled = true;
 }
+
+window.onload = function() {
+    const callClick = window.document.getElementById("clickButton");
+    callClick.addEventListener("click", openSesame);
+}
+
+module.exports = openSesame;
--- test/test.js
+++ test/test.js
@@ -1,10 +1,27 @@
 // This tests the click-script.js to make sure the right output is displayed
-const assert = require("assert");
-const { openSesame } = require("../js");
+
+const fs = require("fs");
+const path = require('path');
+const html = fs.readFileSync(path.resolve(__dirname,"../index.html").toString(), 'utf8');
+
+jest
+    .dontMock('fs');
 
 describe("Button Test", function() {
-    it("checks if button text is correct", function(){
-        assert.isString(openSesame());
-        assert.equal(openSesame(), "Thank you for considering my application!");
+    beforeEach(() => {
+	require('../js/click-script.js');
+        window.document.body.innerHTML = html.toString()
+	const clickMessage = window.document.getElementById('clickMessage');
+        const clickButton = window.document.getElementById('clickButton');
+	clickButton.click();
+    });
+    afterEach(() => {
+        jest.resetModules();
+    });
+    it("Button Click Test", function() {
+        expect(clickMessage.innerHTML).toBe("Thank you for considering my application!");
+    });
+    it("Button Click Test", function() {
+        expect(clickButton.disabled).toBe(true);
     });
 });
